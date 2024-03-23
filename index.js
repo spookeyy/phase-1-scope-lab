5ada88f36e3033d647a84f61af9c1bd410cdb683
spookeyy <pangasmeshack@gmail.com>
2024-03-23 21:45:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Tess'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Chel'
+}
